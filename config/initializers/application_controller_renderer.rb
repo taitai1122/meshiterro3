ea60d18a4068ad9183e1f3286dcab6939a4b59ea
EC2 Default User <taikicody592@icloud.com>
2021-09-21 06:34:00 +0000

    Initial commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '227bc5d904515a4758a72e90d38a3a3b3d260b591169e343968227c8f542076e512339ed9629a2fcd7c8a1fd6c978320edd455bf493193e9f86955fe223ae6b3'
